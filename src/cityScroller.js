0e6d0c93ee00c8cbc084a4799ee9b754d6b9372e
Jacob Jeong <jjeong2020@chadwickschool.org>
2018-03-23 11:49:46 -0700

    Add gif

--- src/cityScroller.js
+++ src/cityScroller.js
@@ -1,123 +1,75 @@
-//since these are variables we don't want to ever change in the code, we capitalize them
 var WINDOW_WIDTH = screen.width;
-var WINDOW_HEIGHT = 400;
-var BACKGROUND_COLOR = 204;
+var WINDOW_HEIGHT = screen.height;
+var BACKGROUND_COLOR = color(255);
 
-Building testBuilding = new Building(0, random(30, 100));
-//TODO: Create an instance of your Skyline object
+Character tmp = new Character();
 
+/* @pjs preload= "tmp-0.gif, tmp-1.gif, tmp-2.gif, tmp-3.gif, tmp-4.gif, tmp-5.gif, tmp-6.gif, tmp-7.gif, tmp-8.gif, tmp-9.gif, tmp-10.gif, tmp-11.gif, tmp-12.gif, tmp-13.gif, tmp-14.gif, tmp-15.gif";*/
+PImage characterImage;
 
-// This function only runs once at the start of the program
+PImage[] character = new PImage[16];
+
 void setup() {
   size(WINDOW_WIDTH, WINDOW_HEIGHT); //sets the size of the window
   frameRate(30); //how many times the draw function is called per second
+  for (var i = 0; i < character.length; i++) {
+    character[i] = loadImage("tmp-" + i + ".gif");
+  }
+    //noLoop();
 }
 
-// called repeatedly
 void draw() {
-  background(BACKGROUND_COLOR); //needed in the draw function to "clear" the screen between updates
-  testBuilding.drawAndUpdate();
-  //TODO: Call drawSkyline on your Skyline object
+  if (keyPressed) {
+    console.log("Pressed a key");
+    tmp.updateCharacterRight();
+  }
+
+tmp.drawCharacter();
 
 }
 
-/**
- * Represents one layer of a city skyline, which is a collection of buildings
- * that all move at the same speed.
- */
-class Skyline {
-  ArrayList < Building > buildingList;
-  var xPosition;
+class Character {
+  var xPos;
+  var characterNumber;
 
-  /**
-   * Constructs a SkyLine with enough buildings to fill the screen
-   */
-  Skyline() {
-    buildingList = new ArrayList < Building > ();
-    xPosition = 0;
-    fillSkyline(); //when a Skyline is created it automatically has enough buildings to fill the screen
+  Character() {
+    xPos = 200;
+    characterNumber = 0;
+  }
+  void moveCharacter() {
+    drawCharacter();
+    updateCharacter();
   }
 
-  void moveSkyline() {
-    //TODO:update and draw the skyline, add buildings as buildings leave the screen
+  void drawCharacter() {
+    image(character[characterNumber], xPos, 475, 100, 100);
   }
 
-  /**
-   * Draws the skyline, placing it on the screen
-   */
-  void drawSkyline() {
-    //TODO:loop through buildingList and draw each Building
+  void updateCharacterLeft() {
+    characterNumber++;
+    if (xPos > WINDOW_WIDTH + 50) {
+      xPos = -50;
+    }
+    if (xPos < -50) {
+      xPos = WINDOW_WIDTH + 50;
+    }
+
+    if (characterNumber == character.length) {
+      characterNumber = 0;
+    }
+    xPos -= 3
   }
-
-  /**
-   * Updates the position of each Building in the SkyLine
-   */
-  void update() {
-    //TODO:loop through buildingList and update each Building
+  void updateCharacterRight() {
+    characterNumber++;
+    if (xPos > WINDOW_WIDTH + 50) {
+      xPos = -50;
+    }
+    if (xPos < -50) {
+      xPosition = WINDOW_WIDTH + 50;
+    }
+    if (characterNumber == character.length) {
+      characterNumber = 0;
+    }
+    xPos += 3
   }
-
-  /**
-   * Adds a building of random building width and then updates
-   * the x position to be the right corner of the building in order
-   * to have the next building not overlap
-   */
-  void addBuilding() {
-    var randomBuildingWidth = random(10, 80);
-    Building currentBuilding = new Building(xPosition, randomBuildingWidth);
-    buildingList.add(currentBuilding);
-    xPosition += randomBuildingWidth;
-  }
-
-
-  void fillSkyline() {
-    //TODO: add enough buildings to fill the screen
-    // hint - use xPosition and WINDOW_WIDTH to figure out when you have
-    //        enough buildings
-    buildingList.add(new Building(0, random(30, 100)));
-  }
-}
-
-/**
-* Represents a building, providing a way to place a building and move
-* it across the screen.
-*/
-class Building {
-
-  var xPosition, yPosition, buildingHeight, buildingWidth;
-  var buildingColor;
-
-  /**
-  * Constructs a Building object
-  * @param xPos - the x position of the top left corner of the building
-  * @param bw - the building's width
-  */
-  Building(var xPos, var bw) {
-    buildingHeight = random(30, 250);
-    buildingWidth = bw;
-    xPosition = xPos;
-    buildingColor = color(0, 0, 255);
-  }
-
-  void drawAndUpdate() {
-    var testSpeed = 5;
-    drawBuilding();
-    update(testSpeed);
-  }
-
-  /**
-   * Draws a building always attached to the bottom of the screen
-   */
-  void drawBuilding() {
-    fill(buildingColor);
-    rect(xPosition, WINDOW_HEIGHT - buildingHeight, buildingWidth, buildingHeight);
-  }
-
-  /**
-   * Updates the x position of the building
-   * @param speed - the speed at which the building updates
-   */
-  void update(var speed) {
-    xPosition += speed;
-  }
-
 }
